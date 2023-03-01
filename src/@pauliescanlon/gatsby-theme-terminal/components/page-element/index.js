a08c5e633e5226ecb7afcd7ddf05b61e130586a8
Fuyuan <zoeblow@gmail.com>
2023-03-01 17:15:24 +0800

    chore: 优化主题

--- src/@pauliescanlon/gatsby-theme-terminal/components/page-element/index.js
+++
@@ -1,1 +0,0 @@
-export { PageElement } from './page-element'
--- src/@pauliescanlon/gatsby-theme-terminal/data/index.js
+++
@@ -1,6 +0,0 @@
-export { useConfig } from './use-config'
-export { useNavigation } from './use-navigation'
-export { useDates } from './use-dates'
-export { useTags } from './use-tags'
-export { useWords } from './use-words'
-export { useSource } from './use-source'
